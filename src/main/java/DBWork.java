172e4ce0ee4366e70bcde519414ec60d44147fa6
AValerievna <a.valerievna12@gmail.com>
2018-12-04 18:10:00 +0300

    full requests and java code drafts

--- src/main/java/DBWork.java
+++ src/main/java/DBWork.java
@@ -1,25 +1,28 @@
+
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 public class DBWork {
 
 
     private final String driverName = "com.mysql.jdbc.Driver";
-    private final String connectionString = "jdbc:mysql://localhost:3306/taskdb";
+    private final String connectionString = "jdbc:mysql://localhost:3306/union_reporting";
     private final String login = "root";
     private final String password = "root";
 
     private final static String TEST_MIN_TIME_REQUEST = "select p.name PROJECT, t.name TEST, min(t.end_time - t.start_time) MIN_WORKING_TIME\n" +
             "from test t join project p on p.id=t.project_id\n" +
             "group by t.name, p.name;";
-    private final static String DISTINCT_TESTS_REQUEST ="select p.name PROJECT, count(distinct t.name) TESTS_COUNT\n" +
+    private final static String DISTINCT_TESTS_REQUEST = "select p.name PROJECT, count(distinct t.name) TESTS_COUNT\n" +
             "from test t join project p on p.id=t.project_id\n" +
             "group by p.name;";
-    private final static String AFTER_DATE_TESTS_REQUEST ="select p.name PROJECT, t.name TEST, t.start_time DATE\n" +
+    private final static String AFTER_DATE_TESTS_REQUEST = "select p.name PROJECT, t.name TEST, t.start_time DATE\n" +
             "from test t join project p on p.id=t.project_id\n" +
             "where start_time >= '2015-07-11 00:00:00'\n" +
             "order by p.name, t.name;";
-    private final static String BROWSER_TEST_COUNT_REQUEST ="select count(name) BROWSERS\n" +
+    private final static String BROWSER_TEST_COUNT_REQUEST = "select count(name) BROWSERS\n" +
             "from test \n" +
             "where browser='firefox'\n" +
             "union\n" +
@@ -27,34 +30,59 @@ public class DBWork {
             "from test \n" +
             "where browser='chrome';\n";
 
-    public static void main(String args[]) throws SQLException, ClassNotFoundException {
+    public static void main(String args[]) throws Exception {
         Logger log = Logger.getGlobal();
         DBWork app = new DBWork();
         Connection dbConnection = app.getConnection();
-        app.getExecutionResult(dbConnection);
+        //app.getExecutionResult(dbConnection, TEST_MIN_TIME_REQUEST);
+        app.getExecutionResult(dbConnection, DISTINCT_TESTS_REQUEST);
+        app.getExecutionResult(dbConnection, AFTER_DATE_TESTS_REQUEST);
+        app.getExecutionResult(dbConnection, BROWSER_TEST_COUNT_REQUEST);
         app.closeConnection(dbConnection);
     }
 
     public Connection getConnection() throws ClassNotFoundException, SQLException {
         Class.forName(driverName);
-        Connection connection = null;
-        connection = DriverManager.getConnection(connectionString, login, password);
+        Connection connection = DriverManager.getConnection(connectionString, login, password);
         return connection;
     }
 
     private void closeConnection(Connection connection) throws SQLException {
-            connection.close();
+        connection.close();
     }
 
 
-
-    private ResultSet getExecutionResult(Connection connection) throws SQLException {
+    private ResultSet getExecutionResult(Connection connection, String query) throws Exception {
         Statement stmt = connection.createStatement();
-        ResultSet rs = stmt.executeQuery("select p.name PROJECT, t.name TEST, min(t.end_time - t.start_time) MIN_WORKING_TIME\n" +
-                "from test t join project p on p.id=t.project_id\n" +
-                "group by t.name, p.name;\n");
-
-        System.out.println(rs);
+        ResultSet rs = stmt.executeQuery(query);
+        System.out.println(fromResultSet(rs));
         return rs;
     }
+
+    public String fromResultSet(ResultSet resultSet) throws SQLException {
+        if (resultSet == null) throw new NullPointerException("resultSet == null");
+        if (!resultSet.isBeforeFirst()) throw new IllegalStateException("Result set not at first.");
+
+        List<String> headers = new ArrayList<>();
+        ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+        int columnCount = resultSetMetaData.getColumnCount();
+        for (int column = 0; column < columnCount; column++) {
+            headers.add(resultSetMetaData.getColumnName(column + 1));
+        }
+
+        List<String[]> data = new ArrayList<>();
+        while (resultSet.next()) {
+            String[] rowData = new String[columnCount];
+            for (int column = 0; column < columnCount; column++) {
+                rowData[column] = resultSet.getString(column + 1);
+            }
+            data.add(rowData);
+        }
+
+        String[] headerArray = headers.toArray(new String[headers.size()]);
+        String[][] dataArray = data.toArray(new String[data.size()][]);
+        return new FlipTable(headerArray, dataArray).toString();
+    }
+
+
 }
